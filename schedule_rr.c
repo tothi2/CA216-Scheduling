d3308d600b830f8a5f805dc944c7662cfaa7290e
Imrich Toth <imrich.toth2@mail.dcu.ie>
2021-04-12 01:13:19 +0100

    Updated schedule_rr.c

--- schedule_rr.c
+++ schedule_rr.c
@@ -5,12 +5,11 @@
 #include "list.h"
 #include "cpu.h"
 
-node_t *tasks = NULL;
+nodetotal *tasks = NULL;
 int unique_id = 0;
 
-// add a task to the list
-void add(char *name, int priority, int burst)
-{
+// adding tasks to the list
+void add(char *name, int priority, int burst) {
     Task *new_task = malloc(sizeof(Task));
     new_task->burst = burst;
     new_task->name = name;
@@ -19,46 +18,36 @@ void add(char *name, int priority, int burst)
     insert(&tasks, new_task);
 }
 
-// sort the tasks in the right order
-node_t *sort(node_t *head)
-{
-    node_t *newlist = NULL;
-    for (node_t *tmp = tasks; tmp; tmp = tmp->next)
-    {
+// making sure the tasks are done in the right order
+nodetotal *sort(node_t *head) {
+    nodetotal *newlist = NULL;
+    for (nodetotal *tmp = tasks; tmp; tmp = tmp->next) {
         insert(&newlist, tmp->task);
     }
     return newlist;
 }
 
-void clear(node_t **head)
-{
+void clear(nodetotal **head) {
     bool still = false;
-    do
-    {
-        node_t *to_rm = NULL;
-        for (node_t *tmp = *head; tmp; tmp = tmp->next)
-        {
-            if (tmp->task->burst == 0)
-            {
+    do {
+        nodetotal *to_rm = NULL;
+        for (nodetotal *tmp = *head; tmp; tmp = tmp->next) {
+            if (tmp->task->burst == 0) {
                 to_rm = tmp;
                 break;
             }
         }
-        if (to_rm != NULL)
-        {
+        if (to_rm != NULL) {
             delete (head, to_rm->task);
         }
         still = to_rm != NULL;
     } while (still);
 }
 
-// invoke the scheduler
-void schedule()
-{
-    while (tasks != NULL)
-    {
-        for (node_t *tmp = tasks; tmp; tmp = tmp->next)
-        {
+
+void schedule() {
+    while (tasks != NULL) {
+        for (nodetotal *tmp = tasks; tmp; tmp = tmp->next) {
             int burst = tmp->task->burst > 10 ? 10 : tmp->task->burst;
             run(tmp->task, burst);
             tmp->task->burst -= burst;
